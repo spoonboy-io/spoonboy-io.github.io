894a844349a17367060578a6a535e1f14433eefa
Ollie Phillips <ollie@interject.io>
2023-06-03 18:00:32 +0100

    Update javascript-snippets.js

--- js/javascript-snippets.js
+++ js/javascript-snippets.js
@@ -1,5 +1,16 @@
-// snippets for Morpheus JavaScript snippets plugin - temporary
+// snippets for Morpheus JavaScript snippets plugin - temporary test location
 
-// simple test
+// make sure we can do stuff in the DOM
+document.addEventListener("DOMContentLoaded", function(event){
+  
+  // simple test
+  console.log("spoonboy snippets!");
+  
+  
+  
 
-console.log("spoonboy snippets!");
+
+
+
+
+});
