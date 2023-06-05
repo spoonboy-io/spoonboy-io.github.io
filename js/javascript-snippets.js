f6df38ca29e74a1342dc3288a651c3695ca41b24
Ollie Phillips <ollie@interject.io>
2023-06-05 08:50:59 +0100

    Update javascript-snippets.js

--- js/javascript-snippets.js
+++ js/javascript-snippets.js
@@ -1,20 +1,21 @@
 // snippets for Morpheus JavaScript snippets plugin - temporary test location
-
-// make sure we can do stuff in the DOM
+// wrap all our snippets so that we have the DOM available to read/manipulate
 document.addEventListener("DOMContentLoaded", function(event){
-  
-  // simple test
-  console.log("snippets are go!");
-  
-  // get the page 
-  let path = window.location.pathname;
-  let page = path.split("/").pop();
-  console.log( page );
-  
-  
+    // allowed pages, we limit the scope of our event handlers to what is needed
+    let namePages = ["provisioning/instances", "provisioning/catalog", "service-catalog/dashboard"];
 
+    // get the current page & prefix
+    let path = window.location.pathname.split("/");
+    let page = path.pop();
+    let prefix =  path.pop();
+    let pagePrefixed = prefix + "/" + page;
 
-
-
-
+    // only on allowed pages
+    if (namePages.includes(pagePrefixed)) {
+        // set up  the trim name field event & handler
+        let nameF = document.getElementById("name");
+        nameF.addEventListener("blur", (event) => {
+            nameF.value = nameF.value.trim()
+        });
+    }
 });
